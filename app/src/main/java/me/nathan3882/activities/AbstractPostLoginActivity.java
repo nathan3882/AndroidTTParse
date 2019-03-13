a9366d5b5ab0079b8b3f2f000ac4f7603563a1f5
nathan3882 <nathan.allanson14@hotmail.com>
2019-03-13 22:33:21 +0000

    util + lesson manipulation

--- app/src/main/java/me/nathan3882/activities/AbstractPostLoginActivity.java
+++ app/src/main/java/me/nathan3882/activities/AbstractPostLoginActivity.java
@@ -3,12 +3,16 @@ package me.nathan3882.activities;
 import android.app.Activity;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 import me.nathan3882.androidttrainparse.User;
-import me.nathan3882.requestsResponses.IActivityReferencer;
+import me.nathan3882.requesting.IActivityReferencer;
+import me.nathan3882.responding.ResponseEvent;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 
-public abstract class AbstractPostLoginActivity extends AppCompatActivity implements IActivityReferencer<Activity> {
+public abstract class AbstractPostLoginActivity extends AppCompatActivity
+        implements IActivityReferencer<Activity> {
 
     abstract User getUser();
 
@@ -18,24 +22,36 @@ public abstract class AbstractPostLoginActivity extends AppCompatActivity implem
         return getUser().getHomeCrs();
     }
 
+    public void showToast(String text, String length, boolean show) {
+        Toast toast = Toast.makeText(this, text, length.equals("short") ? Toast.LENGTH_SHORT : Toast.LENGTH_LONG);
+        if (show) toast.show();
+    }
+
     @Nullable
     public String getUsersEmail() {
         return getUser().getUserEmail();
     }
 
-    public void addLesson(String lesson) {
-        getUser().addLesson(lesson);
+    public void addLesson(String lesson, ResponseEvent event) {
+        getUser().addLesson(lesson, event);
     }
 
-    public void removeLesson(String lesson) {
-        getUser().removeLesson(lesson);
+    public void addLessonProgressed(String lesson, WeakReference<Activity> reference, ProgressBarable barable, ResponseEvent event) {
+        getUser().addLessonProgressed(lesson, reference, barable, event);
+    }
+
+    public void removeLesson(String lesson, ResponseEvent event) {
+        getUser().removeLesson(lesson, event);
+    }
+
+    public void removeLessonProgressed(String lesson, WeakReference<Activity> reference, ProgressBarable barable, ResponseEvent event) {
+        getUser().removeLessonProgressed(lesson, reference, barable, event);
     }
 
     public void synchroniseWithDatabase() {
-        getUser().uploadLessons();
+        getUser().syncWithDatabase();
     }
 
-    @Nullable
     public ArrayList<String> getUsersLessons(boolean freshRequest) {
         return getUser().getLessons();
     }
