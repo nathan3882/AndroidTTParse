7d5ba96ddd947df638cb002fe1252fb4316140a3
nathan3882 <nathan.allanson14@hotmail.com>
2019-03-14 22:29:00 +0000

    reformatted

--- app/src/main/java/me/nathan3882/requesting/KeyObjectPair.java
+++ app/src/main/java/me/nathan3882/requesting/KeyObjectPair.java
@@ -2,19 +2,19 @@ package me.nathan3882.requesting;
 
 public class KeyObjectPair {
 
-    private final Object value;
     private final String key;
+    private final Object value;
 
     public KeyObjectPair(String key, Object value) {
         this.key = key;
         this.value = value;
     }
 
-    public Object getValue() {
-        return value;
-    }
-
     public String getKey() {
         return key;
+    }
+
+    public Object getValue() {
+        return value;
     }
 }
