7dbc9083f13d7fbd1451e5a2d399d194f5c3ff69
nathan3882 <nathan.allanson14@hotmail.com>
2019-03-15 21:24:02 +0000

    func interface for referencing progress bars

---
+++ app/src/main/java/me/nathan3882/activities/ProgressBarable.java
@@ -0,0 +1,27 @@
+package me.nathan3882.activities;
+
+import android.app.Activity;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ProgressBar;
+
+@FunctionalInterface
+public interface ProgressBarable {
+
+    @Nullable
+    default ProgressBar getProgressBar(Activity activity, int id) {
+        return activity.findViewById(id);
+    }
+
+    int getProgressBarRid();
+
+    default void doProgress(ProgressBar bar, int newProgress) {
+        if (bar != null) {
+            if (bar.getVisibility() == View.INVISIBLE || bar.getVisibility() == View.GONE) {
+                bar.setVisibility(View.VISIBLE);
+            }
+            bar.setProgress(newProgress);
+        }
+    }
+
+}
