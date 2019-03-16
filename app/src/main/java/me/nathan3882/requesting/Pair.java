56ca207d7d576b4a4eea8157d3f3c030d86fcbc2
nathan3882 <nathan.allanson14@hotmail.com>
2019-03-16 17:44:23 +0000

    superclasses Pair for different types of Pairs

---
+++ app/src/main/java/me/nathan3882/requesting/Pair.java
@@ -0,0 +1,55 @@
+package me.nathan3882.requesting;
+
+import me.nathan3882.responding.ResponseEvent;
+
+public class Pair {
+
+    protected Object key;
+    protected Object value;
+
+    public Object getKey() {
+        return key;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public class BooleanResponseEventPair extends Pair {
+
+        public BooleanResponseEventPair(Boolean key, ResponseEvent value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public ResponseEvent getValue() {
+            return (ResponseEvent) value;
+        }
+
+        @Override
+        public Boolean getKey() {
+            return (Boolean) key;
+        }
+    }
+
+    public class KeyObjectPair extends Pair {
+
+        public KeyObjectPair(String key, Object value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String getKey() {
+            return (String) super.key;
+        }
+
+        @Override
+        public Object getValue() {
+            return super.value;
+        }
+    }
+
+
+}
